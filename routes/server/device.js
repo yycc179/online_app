c1c83dc04aed6daed6a8f76dd51b16a70366b61c
yy <yycc179@gmail.com>
2022-01-05 10:08:06 +0800

    add ip loc

--- routes/server/device.js
+++ routes/server/device.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
     , moment = require('moment-timezone')
     , utils = require('./utils')
     , Device = require('../../models/device')
+    , Devinfo = require('../../models/devinfo')
 
 router.get('/', utils.getClientIp, (req, res) => {
     const { chipid = 'unknown', ip = req.ip } = req.query;
@@ -34,5 +35,33 @@ router.get('/', utils.getClientIp, (req, res) => {
 
 })
 
+router.get('/info', utils.getClientIp, (req, res) => {
+    const { ip = req.ip } = req.query;
+
+    Devinfo.findOne({ ip })
+        .exec((e, dev) => {
+            if (e) {
+                res.json({ err: "error to find: " + ip });
+            }
+            else if (!dev) {
+                utils.queryCity(ip).then(data => {
+                    data.ip = ip;
+                    new Devinfo(data).save();
+                    data.time = moment().tz(data.timezone).format()
+                    res.json(data);
+                })
+
+            }
+            else {
+                const { country, city, region, ll, timezone, ip } = dev
+                res.json({
+                    country, city, region, ll, timezone, ip,
+                    time: moment().tz(timezone).format()
+                });
+            }
+
+        })
+
+})
 
 module.exports = router;
--- routes/server/resource.js
+++ routes/server/resource.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
     , Stats = require('../../models/stats')
     , Utils = require('./utils')
-    , Device = require('../../models/device')
+    , Devinfo = require('../../models/devinfo')
+
 
 router.get('/:name/*/*.bin', Utils.getClientIp, (req, res, next) => {
     const { name } = req.params
     const { ip } = req;
 
     var date = Utils.getCurrentMounth()
-    Device.findOne({ ip }).exec((e, d) => {
+    Devinfo.findOne({ ip }).exec((e, d) => {
         const country  = d ? d.country : 'Unknow'
        Stats.updateOne({ date, name, country}, { $inc: { viewCount: 1 } }, { upsert: true }).exec()
     })
